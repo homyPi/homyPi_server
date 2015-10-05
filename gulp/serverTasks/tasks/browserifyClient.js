ef56bcce3f8ad39f0b7785659eae55cde641a5c8
Youenn PENNARUN <youenn.pennarun@gmail.com>
2015-10-05 19:53:36 +0200

    fix gulp task html; set npm start

---
+++ gulp/serverTasks/tasks/browserifyClient.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var connect = require('gulp-connect');
+var config = require('../../clientTasks/config').browserify;
+
+watchify.args.debug = config.debug;
+var bundler = watchify(browserify(config.src, watchify.args));
+config.settings.transform.forEach(function(t) {
+  bundler.transform(t);
+});
+
+gulp.task('browserifyClient', bundle);
+
+function bundle() {
+  return bundler.bundle()
+  // log errors if they happen
+  .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+  .pipe(source(config.outputName))
+  .pipe(gulp.dest(config.dest))
+}
