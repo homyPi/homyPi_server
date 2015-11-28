2f7423beb7847db239b5ec529a5a89e01b9d7176
Youenn PENNARUN <youenn.pennarun@gmail.com>
2015-11-28 01:34:59 +0100

    removed config.js from .gitignore (fixing tests)

--- src/server/test/testModules/dependencyTest/server/config.js
+++ src/server/test/testModules/dependencyTest/server/config.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 module.exports = {
-	"require": [{module: "dependencyTest", version: "0.1"}]
+	"require": [{module: "moduleSimple", version: "0.1"}]
 };
